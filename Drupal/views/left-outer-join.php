3e1762876af328ea87e79d1f7c8a6daa8c3d19dc
Andy Truong <andytruong@users.noreply.github.com>
2014-12-10 08:46:04 +0700

    Update left-outer-join.php

--- Drupal/views/left-outer-join.php
+++ Drupal/views/left-outer-join.php
@@ -16,7 +16,7 @@ namespace Drupal\my_module\ViewsHandler extends \views_handler_field {
   class MyCustomField {
     function query() {
       $table = $this->query->relationships['my_table']['table'];
-      $join = new views_join_complex();
+      $join = new \views_join_complex();
       $join->construct('my_other_table', $table, 'my_column', 'my_column', array(), 'LEFT OUTER');
       $alias = $this->query->add_table('my_other_table', NULL, $join, "{$table}_other");
       $this->field_alias = $this->query->add_field(NULL, "{$alias}.my_column IS NOT NULL", "{$alias}_my_column");
